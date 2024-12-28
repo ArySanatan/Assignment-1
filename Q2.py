e6191847127869a801dd74f8ec845fdefdaaf6c1
ArySanatan <sanatan14121@gmail.com>
2024-12-28 18:04:37 +0530

    Update Q2.py

---
+++ Q2.py
@@ -0,0 +1,11 @@
+# Write a Python program that checks a number divisible by 3 and 2.
+
+print("Enter Number")
+Num=int(input()) 
+
+#conditions
+if(Num%3==0):
+    if(Num%2==0): print(Num,"is divisible by 3 and 2")
+    else: print(Num,"is not divisible by 3 and 2")
+    
+else: print(Num,"is not divisible by 3 and 2")
