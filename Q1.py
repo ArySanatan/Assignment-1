721cca05bcf666a16375d5852aef269173e23c67
ArySanatan <sanatan14121@gmail.com>
2024-12-28 17:54:14 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,1 +1,11 @@
-# 
+'''Write a Python program that takes input as an integer from the user and checks
+whether it is even or odd. '''
+
+print('Enter an Integer :',)
+a=int(input())  #taking user input 
+if(a%2==0):{
+    print(a,"is even")
+}
+else:
+    print(a,"is odd")
+
