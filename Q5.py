7f4dd4f25de1ca43708a7be56d58768cbe4cd7c5
ArySanatan <sanatan14121@gmail.com>
2024-12-28 18:40:34 +0530

    Q5

---
+++ Q5.py
@@ -0,0 +1,7 @@
+'''Write a Python program that takes a number as input and checks if it is a 3-digit
+number.'''
+
+num=int(input("Enter a Number : "))
+
+if (num>99 and num<1000): print("It is a 3-digit Number")
+else: print("No, Its not a 3-digit number")
